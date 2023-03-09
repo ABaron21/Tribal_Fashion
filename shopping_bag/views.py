1609382dd67a39e2357bbeed3926d00c212ad789
A_Baron <abaron756@gmail.com>
2023-03-09 20:20:13 +0000

    Add functionality for users to add product to their shopping bag and display the shopping bag items in the shopping bag view

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,18 @@ from django.shortcuts import render
 def view_shopping_bag(request):
     template = 'shopping_bag/view_bag.html'
     return render(request, template)
+
+
+def add_to_bag(request, product_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+    
+    request.session['bag'] = bag
+    return redirect(redirect_url)
