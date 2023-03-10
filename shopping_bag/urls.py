043bae4b806e4df024f261e7be86c9800b2b5cae
A_Baron <abaron756@gmail.com>
2023-03-10 15:07:50 +0000

    Add functionality to allow users to alter an item within their shopping bag

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.view_shopping_bag, name='view_bag'),
     path('add_to_bag/<int:product_id>', views.add_to_bag, name='add_to_bag'),
+    path('adjust_quantity/<product_id>', views.adjust_quantity,
+         name='adjust_quantity'),
+    path('remove_item/<product_id>', views.remove_item,
+         name='remove_item'),
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
 
 def view_shopping_bag(request):
+    print(request.session['bag'])
     template = 'shopping_bag/view_bag.html'
     return render(request, template)
 
@@ -18,6 +19,30 @@ def add_to_bag(request, product_id):
         bag[product_id] += quantity
     else:
         bag[product_id] = quantity
-    
+
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_quantity(request, product_id):
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[product_id] = quantity
+    else:
+        bag.pop(product_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_item(request, product_id):
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(product_id)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
