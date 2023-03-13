c912869c0455e01a0401f3fbec2da285586cd389
A_Baron <abaron756@gmail.com>
2023-03-13 10:10:04 +0000

    Fix issue with product details view overwriting an items qty if the item exists in the bag

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -6,7 +6,6 @@ from products.models import Product
 
 
 def view_shopping_bag(request):
-    print(request.session['bag'])
     template = 'shopping_bag/view_bag.html'
     return render(request, template)
 
@@ -17,9 +16,10 @@ def add_to_bag(request, product_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    product_id_str = str(product_id)
 
-    if product_id in list(bag.keys()):
-        bag[product_id] += quantity
+    if product_id_str in list(bag.keys()):
+        bag[product_id_str] += quantity
         messages.success(request,
                          f'Updated {product.name} quantity to {bag[product_id]}')
     else:
